18ed519a0922d6633f51d0fd111ebb7c3da643f9
M316337 <jandradx@emeal.nttdata.com>
2023-08-25 11:28:56 -0300

    initial commit

---
+++ src/main/java/com/queijin/QueijinApplication.java
@@ -0,0 +1,13 @@
+package com.queijin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueijinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueijinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queijin/QueijinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queijin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueijinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
